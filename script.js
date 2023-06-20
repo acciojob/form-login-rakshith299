6d7e8b2a9526c0575e62a4af957c3f9d4a463fb9
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-20 11:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
-    //Write your code here
+    document.getElementById("form1").addEventListener("submit", function(e){
+		e.preventDefault();
+	})
+
+	const form =  document.getElementById("form1");
+
+	const fName = form.fname.value;
+	const lName = form.lname.value;
+
+	alert(fName + " " + lName);
 
 }
